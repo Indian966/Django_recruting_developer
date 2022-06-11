588a9cc21baf7b5a21f3b8e1837c31e746f9d23a
Indian966 <rlaalsrb4175@gmail.com>
2022-06-11 19:14:24 +0900

    models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Info(models.Model) :
+    user_id = models.CharField(max_length = 100)
+    gender = models.CharField(max_length = 10)
+
+# class InfoB(models.Model) :
+#     user = models.ForeignKey(InfoA, on_)
