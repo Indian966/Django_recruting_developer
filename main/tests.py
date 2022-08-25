4a38722feea88a430e774a72944593ee5418c77e
Indian966 <rlaalsrb4175@gmail.com>
2022-08-25 17:17:39 +0900

    test case 작성 시작

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from main.models import Post
 # Create your tests here.
+
+class CreatePostTestCase(TestCase) :
+
+    def setUpTestData(cls):
+
+        pass
+
+    def test_post(self):
+        pass
