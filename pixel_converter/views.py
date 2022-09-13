17537ccb064c8c6db26546d7d1944cc11fed2006
Indian966 <rlaalsrb4175@gmail.com>
2022-09-13 16:30:47 +0900

    file 업로드 media 경로 지정

--- pixel_converter/views.py
+++ pixel_converter/views.py
@@ -16,6 +16,35 @@ def index(request) :
     if request.method == 'GET' :
         return render(request,'pixel.html')
 
+    elif request.method == 'POST' :
+        img = request.FILES['image']
+        if not img:
+            error='파일을 선택하십시오.'
+            return render('pixel.html', error=error)
+        k = int(request.form['k'])
+        scale = int(request.form['scale'])
+        blur = int(request.form['blur'])
+        erode = int(request.form['erode'])
+        try:
+            alpha = bool(int(request.form['alpha']))
+        except:
+            alpha = False
+        try:
+            to_tw = bool(int(request.form['to_tw']))
+        except:
+            to_tw = False
+        img_name = hashlib.md5(str(dt.datetime.now()).encode('utf-8')).hexdigest()
+        img_path = os.path.join('media/pixel_converter/img', img_name + os.path.splitext(img.filename)[-1])
+        result_path = os.path.join('media/pixel_converter/results', img_name + '.png')
+        img.save(img_path)
+        with Image.open(img_path) as img_pl:
+            if max(img_pl.size) > 1024:
+                img_pl.thumbnail((1024, 1024), Image.ANTIALIAS)
+                img_pl.save(img_path)
+        img_res, colors = make_dot(img_path, k=k, scale=scale, blur=blur, erode=erode, alpha=alpha, to_tw=to_tw)
+        cv2.imwrite(result_path, img_res)
+        return render('pixel.html', org_img=img_path, result=result_path, colors=colors)
+
 
 # def post(request):
 #     img = request.files['image']
