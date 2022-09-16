0ae71b7688cb6fa1a5242621e30b6336b992a431
Indian966 <rlaalsrb4175@gmail.com>
2022-09-16 17:14:38 +0900

    index 테스트문 작성 중

--- pixel_converter/tests.py
+++ pixel_converter/tests.py
@@ -1,3 +1,14 @@
+import unittest
+import views
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestGenerateMap(unittest.TestCase):
+    def test_index(self):
+        # requset =
+        T = views.index()
+        expect = 0
+        result = T
+        self.assert_()
